3a8b7c7823f98ac6e2560f67e73e54153c03a47c
Maquito365 <macsoloj@gmail.com>
2026-01-16 08:42:45 -0600

    Creación de clases para el programa

---
+++ Radio.java
@@ -0,0 +1,10 @@
+public interface Radio {
+
+    void encenderRadio();
+    void apagarRadio();
+    void avanzarEstacion();
+    void guardarEstacion(int numeroBoton);
+    void crgarEstacion(int numeroBoton);
+    void cambiarFM();
+    void cambiarAM();
+}
